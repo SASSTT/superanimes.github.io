d60f80e7b4538b5576e48f4700b13fe6a91eb62d
SASSTT <132719961+SASSTT@users.noreply.github.com>
2023-06-24 10:21:23 -0300

    code

---
+++ code/test.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>test.php</title>
+    <link rel="shortcut icon" href="../imagens/logo-icon-72-72.png" type="image/x-icon">
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+    <?php
+        $a = 1;
+        echo $a . $a;
+    ?>
+</body>
+</html>
